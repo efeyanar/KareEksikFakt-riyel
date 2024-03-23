ede05f8601f19aefd41e289d86a85fdb18658c08
Efe Yanar <efe1yanar@gmail.com>
2024-03-23 15:41:40 +0300

    Add files via upload

---
+++ KareEksikFaktöriyel/KareEksikFaktöriyel/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+
+tepe:
+Console.Write("Bir Sayı Giriniz : ");
+try
+{
+    double sayi = Convert.ToDouble(Console.ReadLine());
+    if (sayi <= 0)
+    {
+        Console.WriteLine("Lütfen Pozitif Değer Giriniz.");
+        Console.WriteLine("");
+        goto tepe;
+    }
+    else
+    {
+        double kare = 0;
+        double faktoriyel = 1;
+        double eksik = 0;
+
+        if (sayi > 5 && sayi <= 10)
+        {
+            kare = sayi * sayi;
+            Console.WriteLine("Sayının karesi = " + kare);
+            Console.WriteLine("Uygulamayı Kapatmak İçin 'ENTER' Basınız.");
+        }
+        else if (sayi <= 5)
+        {
+            for (double i = 1; i <= sayi; i++)
+            {
+                faktoriyel *= i;
+
+            }
+            Console.WriteLine("Sayının faktöriyeli = " + faktoriyel);
+            Console.WriteLine("Uygulamayı Kapatmak İçin 'ENTER' Basınız.");
+        }
+        else if (sayi > 10)
+        {
+            eksik = (sayi / 2) - 1;
+            Console.WriteLine("Sayının yarısının bir eksiği = " + eksik);
+            Console.WriteLine("Uygulamayı Kapatmak İçin 'ENTER' Basınız.");
+        }
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine("Bilgileri Doğru Girdiğinizden Emin Olunuz.");
+    Console.WriteLine("");
+    goto tepe;
+}
+Console.ReadLine();
